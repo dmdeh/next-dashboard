4ceb2ffe2442cafd9e0595f3e93aefe9e853e880
dmdeh <131264106+dmdeh@users.noreply.github.com>
2024-12-31 17:36:24 +0900

    fix: 주석fix

--- app/dashboard/(overview)/loading.tsx
+++ app/dashboard/(overview)/loading.tsx
@@ -6,4 +6,3 @@ export default function Loading() {
 
 // return <div>Loading...</div>;
 // loading.tsxSuspense를 기반으로 구축된 특수한 Next.js 파일로, 페이지 콘텐츠가 로드되는 동안 대체 UI로 표시되는 폴백 UI를 생성할 수 있습니다.
-// DashboardSkeleton 개쩐다
